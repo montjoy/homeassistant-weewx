b575c8eb4fa6b1348f07316cdf75416e62ac0b23
Jeff Gibson <montjoy@boxcar.org>
2022-01-23 00:10:15 -0600

    Add a little bit of output to run.sh so we can see what step it is on

--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ export TIMEZONE
 echo "The current time is `date`"
 
 cd /home/weewx/public_html
+echo "Starting the python web server..."
 python3 -m http.server 8099 &
 
 cd /home/weewx
+echo "Starting weewxd..."
 /home/weewx/bin/weewxd
